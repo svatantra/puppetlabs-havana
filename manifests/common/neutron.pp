ec70dcf3d26c56a1b17b29360879a5e2db6e1d3c
svatantra <svatantra.liberte@gmail.com>
2014-03-07 12:21:20 +0530

    Update neutron.pp

--- manifests/common/neutron.pp
+++ manifests/common/neutron.pp
@@ -6,8 +6,8 @@
 class havana::common::neutron {
   $controller_management_address = hiera('havana::controller::address::management')
 
-  $data_network = hiera('havana::network::data')
-  $data_address = ip_for_network($data_network)
+  #$data_network = hiera('havana::network::data')
+  #$data_address = ip_for_network($data_network)
 
   class { '::neutron':
     rabbit_host        => $controller_management_address,
@@ -18,16 +18,31 @@ class havana::common::neutron {
   }
 
   # everone gets an ovs agent (TODO true?)
-  class { '::neutron::agents::ovs':
-    enable_tunneling => 'True',
-    local_ip         => $data_address,
-    enabled          => true,
-    tunnel_types     => ['gre',],
-  }
+  #class { '::neutron::agents::ovs':
+  #  enable_tunneling => 'True',
+  #  local_ip         => $data_address,
+  #  enabled          => true,
+  #  tunnel_types     => ['gre',],
+  #}
 
   # everyone gets an ovs plugin (TODO true?)
-  class  { '::neutron::plugins::ovs':
-    sql_connection      => $::havana::resources::connectors::neutron,
-    tenant_network_type => 'gre',
+  #class  { '::neutron::plugins::ovs':
+  #  sql_connection      => $::havana::resources::connectors::neutron,
+  #  tenant_network_type => 'gre',
+  #}
+  
+  # Need to externalize the mappings
+  class { '::neutron::agents::ovs':
+    physical_interface_mappings => 'physnet1:eth2'
   }
+  
+  #everyone gets an Linux Bridge Plugin 
+  #Need to externalize the vlan ranges
+  class  { '::neutron::plugins::linuxbridge':
+    sql_connection      => $::havana::resources::connectors::neutron,
+    tenant_network_type => 'vlan',
+    network_vlan_ranges => 'physnet1:1000:2000',
+  }
+  
+  
 }
