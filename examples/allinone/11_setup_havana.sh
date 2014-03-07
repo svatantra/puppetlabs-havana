ccfb5e9d6c5b1e92d67167dd4f37f84812b939c4
svatantra <svatantra.liberte@gmail.com>
2014-03-07 15:13:05 +0530

    Update 11_setup_havana.sh

--- examples/allinone/11_setup_havana.sh
+++ examples/allinone/11_setup_havana.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Move the Havana module to the Puppet Master
-vagrant ssh puppet -c "cd /etc/puppet/modules; \
-sudo tar -xvzf /vagrant/*gz; \
-sudo rm -rf havana
-sudo mv puppetlabs-havana* havana; \
-sudo cp /vagrant/hiera.yaml /etc/puppet/hiera.yaml; \
+"cd /etc/puppet/modules; \
+sudo cp /etc/puppet/modules/havana/examples/hiera.yaml /etc/puppet/hiera.yaml; \
 sudo chown root:puppet /etc/puppet/hiera.yaml; \
 sudo mkdir /etc/puppet/hieradata; \
 sudo chown root:puppet /etc/puppet/hieradata; \
